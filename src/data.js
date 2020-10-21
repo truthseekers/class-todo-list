523859e7a72bb4888c47c6e83c1d6852c10fd69b
John Curry <john@truthseekers.io>
2020-10-21 15:15:26 -0700

    done with this super explanation

---
+++ src/data.js
@@ -0,0 +1,21 @@
+const initialTodos = [
+  {
+    id: 0,
+    title: "Buy Radishes",
+    isCompleted: true,
+  },
+  {
+    id: 1,
+    title: "Charge phone",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    title: "Eat a carrot",
+    isCompleted: false,
+  },
+];
+
+module.exports = {
+  initialTodos,
+};
